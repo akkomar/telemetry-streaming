3112bcdfd82bedef4429e20eeb31bae7d19e70d0
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2018-07-24 19:13:15 +0200

    Add delta test job

---
+++ databricks-deploy/update_experiment_enrollment_aggregates_delta_streaming_job.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -eux pipefail
+
+source "$(dirname "$0")"/set_env.sh
+
+OUTPUT_PATH=s3://mozilla-databricks-telemetry-test/enrollment_aggregates_delta
+CHECKPOINT_PATH=s3://mozilla-databricks-telemetry-test/enrollment_aggregates_delta_checkpoint
+
+
+create_job_json() {
+cat << EOF
+{
+    "name": "Experiment enrollment aggregates - streaming",
+    "new_cluster": {
+        "spark_version": "4.1.x-scala2.11",
+        "node_type_id": "c3.2xlarge",
+        "aws_attributes": {
+            "first_on_demand": "1",
+            "availability": "SPOT_WITH_FALLBACK",
+            "instance_profile_arn": "${IAM_ROLE}",
+            "zone_id": "us-west-2b"
+        },
+        "autoscale": {
+            "min_workers": 1,
+            "max_workers": 10
+        },
+        "ssh_public_keys": ["${SSH_PUBLIC_KEY}"],
+        "spark_conf": {
+            "spark.metrics.namespace": "telemetry-streaming",
+            "spark.metrics.conf.*.sink.statsd.class": "org.apache.spark.metrics.sink.StatsdSink",
+            "spark.sql.streaming.metricsEnabled": "true",
+            "spark.databricks.delta.preview.enabled": "true"
+        },
+        "custom_tags": {
+            "App": "data",
+            "Env": "databricks-dev",
+            "Type": "streaming",
+            "TelemetryJobName": "experiment_enrollments_delta"
+        }
+    },
+    "libraries": [{"jar": "s3://net-mozaws-data-us-west-2-ops-ci-artifacts/mozilla/telemetry-streaming/delta-test/telemetry-streaming.jar"}],
+    "email_notifications": {
+        "on_start": ["akomarzewski@mozilla.com"],
+        "on_success": ["akomarzewski@mozilla.com"],
+        "on_failure": ["akomarzewski@mozilla.com"]
+    },
+    "spark_jar_task": {
+        "main_class_name": "com.mozilla.telemetry.streaming.ExperimentEnrollmentsAggregator",
+        "parameters": ["--kafkaBroker","${KAFKA_BROKER}", "--outputPath","${OUTPUT_PATH}", "--checkpointPath","${CHECKPOINT_PATH}"]
+    }
+}
+EOF
+}
+
+databricks jobs reset --job-id 483 --json "$(create_job_json)"
